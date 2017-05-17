8ab1f4089d81807d0cab6f0fea25e95304cc5202
John Muth <john.muth@mergermarket.com>
2017-05-17 13:00:57 +0100

    use scanner from bufio

--- phrasetokenize.go
+++ phrasetokenize.go
@@ -1,21 +1,70 @@
 package main
 
 import (
+	"bufio"
 	"strings"
-	"text/scanner"
+	"unicode"
 )
 
-func Phrasetokenize(input string) (tokens []string) {
-	tokens = []string{}
-	var s scanner.Scanner
-	s.Init(strings.NewReader(input))
-	var tok rune
-	for tok != scanner.EOF {
-		tok = s.Scan()
-		//fmt.Println("At position", s.Pos(), ":", s.TokenText())
-		if len(s.TokenText()) > 0 {
-			tokens = append(tokens, s.TokenText())
+func phrasetokenize(input string) (terms []string, phrases []string) {
+	bs := bufio.NewScanner(strings.NewReader(input))
+	bs.Split(bufio.ScanWords)
+	var currentPhrase []string
+	for bs.Scan() {
+		if bs.Text()[:1] == `"` {
+			if bs.Text()[len(bs.Text())-1:] == `"` {
+				terms = append(terms, removePunctuation(bs.Text()))
+			} else {
+				currentPhrase = append(currentPhrase, removePunctuation(bs.Text()))
+			}
+		} else if bs.Text()[len(bs.Text())-1:] == `"` && len(currentPhrase) > 0 {
+			currentPhrase = append(currentPhrase, removePunctuation(bs.Text()))
+			phrases = append(phrases, strings.Join(currentPhrase, " "))
+			currentPhrase = []string{}
+		} else if len(currentPhrase) > 0 {
+			currentPhrase = append(currentPhrase, removePunctuation(bs.Text()))
+		} else {
+			terms = append(terms, removePunctuation(bs.Text()))
+		}
+	}
+	if len(currentPhrase) > 0 {
+		for _, term := range currentPhrase {
+			terms = append(terms, term)
 		}
 	}
 	return
+}
+
+func removePunctuation(input string) (output string) {
+	var listOfPunct []string
+	for c := range RunesFromRange(unicode.Punct) {
+		// the Latin runes category < 0x600
+		if c < 0x600 {
+			listOfPunct = append(listOfPunct, string(c))
+		}
+	}
+	for _, punct := range listOfPunct {
+		if strings.Contains(input, punct) {
+			input = strings.Replace(input, punct, "", -1)
+		}
+	}
+	return input
+}
+
+func RunesFromRange(tab *unicode.RangeTable) <-chan rune {
+	res := make(chan rune)
+	go func() {
+		defer close(res)
+		for _, r16 := range tab.R16 {
+			for c := r16.Lo; c <= r16.Hi; c += r16.Stride {
+				res <- rune(c)
+			}
+		}
+		for _, r32 := range tab.R32 {
+			for c := r32.Lo; c <= r32.Hi; c += r32.Stride {
+				res <- rune(c)
+			}
+		}
+	}()
+	return res
 }
--- phrasetokenize_test.go
+++ phrasetokenize_test.go
@@ -1,39 +1,58 @@
 package main
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 var testCases = []struct {
-	input          string
-	expectedOutput []string
-	message        string
+	input           string
+	expectedTerms   []string
+	expectedPhrases []string
+	message         string
 }{
 	{
 		"",
-		[]string{},
+		nil,
+		nil,
 		"empty string",
 	},
 	{
 		"hello",
 		[]string{"hello"},
+		nil,
 		"single one-word string",
 	},
 	{
 		"hello world",
-		[]string{"hello","world"},
+		[]string{"hello", "world"},
+		nil,
 		"two one-word strings",
 	},
 	{
 		`"Hello world," she said`,
-		[]string{`"Hello world,"`, "she", "said"},
+		[]string{"she", "said"},
+		[]string{"Hello world"},
 		"a phrase and two words",
+	},
+	{
+		`"Hello," she said`,
+		[]string{"Hello", "she", "said"},
+		nil,
+		"single word in quotes is a term",
+	},
+	{
+		`she said "Hello how`,
+		[]string{"she", "said", "Hello", "how"},
+		nil,
+		"unclosed quotes, is a term",
 	},
 }
 
 func TestPhrasetokenize(t *testing.T) {
 	for _, tc := range testCases {
-		assert.Equal(t, tc.expectedOutput, Phrasetokenize(tc.input), tc.message)
+		terms, phrases := phrasetokenize(tc.input)
+		assert.Equal(t, tc.expectedTerms, terms, tc.message)
+		assert.Equal(t, tc.expectedPhrases, phrases, tc.message)
 	}
 }
